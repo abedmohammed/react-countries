b7de559022bd8f7c7b4e8aaef41f19a02475d0c0
abedm <abedmohammed353@gmail.com>
2023-02-24 10:21:53 -0500

    Initial sass setup and folder structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.scss";
+import "./sass/main.scss";
+
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
